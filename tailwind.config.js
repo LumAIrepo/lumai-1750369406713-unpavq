63e8124e3a9bbe824d0a3f95e9375046064b85bd
LumAI <zurichixbt@gmail.com>
2025-06-19 23:43:34 +0200

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+      },
+      fontFamily: {
+        sans: ["var(--font-Inter)", "system-ui"],
+      },
+      borderRadius: {
+        lg: "0.5remrem",
+        md: "calc(0.5remrem - 0.2rem)",
+        sm: "calc(0.5remrem - 0.4rem)",
+      },
+      spacing: {
+        unit: "undefinedpx",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
